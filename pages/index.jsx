3298da36015b81a9ba183b7f00333dc4966e5ab1
F ReyeSFT <fernandoreyezpinto@gmail.com>
2023-01-05 11:17:16 -0500

    add section skills to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar";
 import AboutMeSection from "../components/aboutMeSection";
+import SkillItems from "../components/skillItems"
 export default function Home() {
   return (
     <>  
       <Navbar></Navbar>
       <AboutMeSection></AboutMeSection>
+      <SkillItems></SkillItems>
     </>
   );
 }
